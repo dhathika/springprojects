f95167f12639ff8da5ff698c670873eb7c186adb
ashokkumarreddyB <bedadalaashokkumarreddy@gmail.com>
2024-06-03 18:56:30 +0530

    committed

---
+++ Springproject/spring-xml-crud-project/src/main/java/com/dhathika/dto/Order.java
@@ -0,0 +1,35 @@
+package com.dhathika.dto;
+
+public class Order {
+	
+	private int orderId;
+	private String orderName;
+	private double orderPrice;
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	
+	public double getOrderPrice() {
+		return orderPrice;
+	}
+	public void setOrderPrice(double orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", orderName=" + orderName + ", orderPrice=" + orderPrice + "]";
+	}
+	
+	
+
+}
