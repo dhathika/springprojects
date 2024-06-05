f4819b3943fd2da7e1ef6a10f8b195a22c69f5c6
csvishnu1998 <thanvivishnu662@gmail.com>
2024-06-05 13:32:13 +0530

    aadhar project is done in SpringProjects

---
+++ spring-xml-project2/src/main/java/com/rohan/util/IncomeLowToHighComparator.java
@@ -0,0 +1,19 @@
+package com.rohan.util;
+
+import java.util.Comparator;
+
+import com.rohan.pojo.Aadhar;
+
+public class IncomeLowToHighComparator implements Comparator<Aadhar>{
+
+	@Override
+	public int compare(Aadhar o1, Aadhar o2) {
+		if(o1.getIncome()<o2.getIncome())
+			return -1;
+		else if(o1.getIncome()>o2.getIncome())
+			return 1;
+		else
+		return 0;
+	}
+
+}
