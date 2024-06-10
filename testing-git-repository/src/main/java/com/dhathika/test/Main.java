6378b8887e8b1a48501910591a95685e26cbd3bd
dhathika <dhathikasuresh@gmail.com>
2024-06-10 06:48:31 +0530

    test commit to renuka branch

---
+++ testing-git-repository/src/main/java/com/dhathika/test/Main.java
@@ -0,0 +1,10 @@
+package com.dhathika.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+System.out.println("today all our session spent on corructing workspaces");
+	}
+
+}
